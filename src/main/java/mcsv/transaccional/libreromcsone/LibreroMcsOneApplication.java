c524d56f707fbba71516e41b741fdd8692e5662f
Erik Dario Hernandez Vasquez <erikdhv@gmail.com>
2023-11-18 23:36:33 -0500

    Commit inicial del Repositorio

---
+++ src/main/java/mcsv/transaccional/libreromcsone/LibreroMcsOneApplication.java
@@ -0,0 +1,13 @@
+package mcsv.transaccional.libreromcsone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreroMcsOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreroMcsOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mcsv/transaccional/libreromcsone/LibreroMcsOneApplicationTests.java
@@ -0,0 +1,13 @@
+package mcsv.transaccional.libreromcsone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreroMcsOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
